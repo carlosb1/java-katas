17e0b2193f9a1f38411e2721adc3717b74e76d63
Carlos B <carlos.baezruiz@gmail.com>
2016-02-28 17:50:33 +0100

    Added answer for kata

--- prime-factors/src/main/java/katas/primefactors/PrimerFactors.java
+++ prime-factors/src/main/java/katas/primefactors/PrimerFactors.java
@@ -1,11 +1,25 @@
 package katas.primefactors;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PrimerFactors {
 
 	public List<Integer> generate(int number) {
-		return null;
+		List<Integer> primeValues = new ArrayList<Integer>();
+
+		if (number <= 1) {
+			return primeValues;
+		}
+
+		for (int candidate = 2; number > 1; candidate++) {
+			while (number % candidate == 0) {
+				number = number / candidate;
+				primeValues.add(candidate);
+			}
+		}
+
+		return primeValues;
 	}
 
 }
--- prime-factors/src/test/java/katas/primefactors/PrimerFactorsTest.java
+++ prime-factors/src/test/java/katas/primefactors/PrimerFactorsTest.java
@@ -1,7 +1,82 @@
 package katas.primefactors;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 import junit.framework.TestCase;
 
 public class PrimerFactorsTest extends TestCase {
+	PrimerFactors primeFactors;
+
+	@Before
+	public void setUp() {
+		primeFactors = new PrimerFactors();
+	}
+
+	@After
+	public void tearDown() {
+		primeFactors = null;
+	}
+
+	@Test
+	public void testPrimerFactors() {
+		List<Integer> values = primeFactors.generate(0);
+		assertTrue(values.isEmpty());
+	}
+
+	@Test
+	public void testOne() {
+		List<Integer> values = primeFactors.generate(1);
+		assertTrue(values.isEmpty());
+	}
+
+	@Test
+	public void testTwo() {
+		List<Integer> values = primeFactors.generate(2);
+		assertEquals(list(2), values);
+	}
+
+	@Test
+	public void testThree() {
+		List<Integer> values = primeFactors.generate(3);
+		assertEquals(list(3), values);
+	}
+
+	@Test
+	public void testFour() {
+		List<Integer> values = primeFactors.generate(4);
+		assertEquals(list(2, 2), values);
+
+	}
+
+	@Test
+	public void testSix() {
+		List<Integer> values = primeFactors.generate(6);
+		assertEquals(list(2, 3), values);
+	}
+
+	@Test
+	public void testEight() {
+		List<Integer> values = primeFactors.generate(8);
+		assertEquals(list(2, 2, 2), values);
+	}
+
+	@Test
+	public void testNine() {
+		List<Integer> values = primeFactors.generate(9);
+		assertEquals(list(3, 3), values);
+	}
+
+	private List<Integer> list(int... values) {
+		List<Integer> listToCompare = new LinkedList<Integer>();
+		for (int value : values) {
+			listToCompare.add(value);
+		}
+		return listToCompare;
+	}
 
 }
