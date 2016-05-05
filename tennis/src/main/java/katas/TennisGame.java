b0d275dfd0ec96cf9a6c9d86d3d130a1c98f3b10
Carlos B <carlos.baezruiz@gmail.com>
2016-05-05 18:05:31 +0200

    Added Initial kata for tennis

---
+++ tennis/src/main/java/katas/TennisGame.java
@@ -0,0 +1,13 @@
+package katas;
+
+/**
+ * Hello world!
+ *
+ */
+public class TennisGame 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ tennis/src/test/java/katas/TennisGameTest.java
@@ -0,0 +1,9 @@
+package katas;
+
+import junit.framework.TestCase;
+
+public class TennisGameTest extends TestCase {
+	public void test() {
+		assertTrue(false);
+	}
+}
