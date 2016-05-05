845ca62db54c91fe581f176d62e763046265dae2
Carlos B <carlos.baezruiz@gmail.com>
2016-05-05 21:09:09 +0200

    General refactorization

--- tennis/src/main/java/katas/TennisGame.java
+++ tennis/src/main/java/katas/TennisGame.java
@@ -1,6 +1,35 @@
 package katas;
 
 public class TennisGame {
+	public class Player {
+		private Points points;
+
+		public Player() {
+			points = Points.ZERO;
+		}
+
+		public Points getPoints() {
+			return points;
+		}
+
+		public void score(Status status) {
+			if (points == Points.ZERO) {
+				points = Points.FIFTEEN;
+			} else if (points == Points.FIFTEEN) {
+				points = Points.THIRTY;
+			} else if (points == Points.THIRTY) {
+				points = Points.FORTY;
+			} else if (points == Points.FORTY) {
+				if (status == Status.DEUCE) {
+					points = Points.ADVANCED;
+				} else {
+					points = Points.WIN;
+				}
+			} else if (points == Points.ADVANCED) {
+				points = Points.WIN;
+			}
+		}
+	}
 
 	public enum Status {
 		DEUCE, ONEPLAYER_WIN, TWOPLAYER_WIN, NOTFINISHED, ONEPLAYER_ADVANCE, TWOPLAYER_ADVANCE;
@@ -10,85 +39,66 @@ public class TennisGame {
 		ZERO, FIFTEEN, THIRTY, WIN, FORTY, ADVANCED
 	}
 
-	// TODO split users in two clases
-	private Points playerOnePoints;
-	private Points playerTwoPoints;
 	private Status status;
 
+	private final Player player1;
+	private final Player player2;
+
 	public TennisGame() {
-		this.playerOnePoints = Points.ZERO;
-		this.playerTwoPoints = Points.ZERO;
+		this.player1 = new Player();
+		this.player2 = new Player();
+
 		this.status = Status.NOTFINISHED;
 	}
 
 	public final Points getPlayerOneScore() {
-		return playerOnePoints;
+		return player1.getPoints();
 	}
 
 	public final Points getPlayerTwoScore() {
-		return playerTwoPoints;
-	}
-
-	private Points sumPoints(Points points) {
-		Points result = Points.ZERO;
-		if (points == Points.ZERO) {
-			result = Points.FIFTEEN;
-		} else if (points == Points.FIFTEEN) {
-			result = Points.THIRTY;
-		} else if (points == Points.THIRTY) {
-			result = Points.FORTY;
-		} else if (points == Points.FORTY) {
-			if (status == Status.DEUCE) {
-				result = Points.ADVANCED;
-			} else {
-				result = Points.WIN;
-			}
-		} else if (points == Points.ADVANCED) {
-			result = Points.WIN;
-		}
-		return result;
-
+		return player2.getPoints();
 	}
 
 	public final void playerOneScore() {
-		playerOnePoints = sumPoints(playerOnePoints);
-		isDeuce();
+		player1.score(status);
 		updateOnePlayer();
+		isDeuce();
+
 	}
 
 	public void playerTwoScore() {
-		playerTwoPoints = sumPoints(playerTwoPoints);
-		isDeuce();
+		player2.score(status);
 		updateTwoPlayer();
+		isDeuce();
 	}
 
 	private void updateOnePlayer() {
-		if (playerOnePoints == Points.ADVANCED) {
+		if (player1.getPoints() == Points.ADVANCED) {
 			status = Status.ONEPLAYER_ADVANCE;
 		}
-		if (playerOnePoints == Points.WIN) {
+		if (player1.getPoints() == Points.WIN) {
 			status = Status.ONEPLAYER_WIN;
 		}
 	}
 
 	private void isDeuce() {
-		if (playerOnePoints == Points.FORTY && playerTwoPoints == Points.FORTY) {
+		if (player1.getPoints() == Points.FORTY && player2.getPoints() == Points.FORTY) {
 			status = Status.DEUCE;
 		}
 	}
 
 	private void updateTwoPlayer() {
-		if (playerTwoPoints == Points.ADVANCED) {
+		if (player2.getPoints() == Points.ADVANCED) {
 			status = Status.TWOPLAYER_ADVANCE;
 		}
 
-		if (playerTwoPoints == Points.WIN) {
+		if (player2.getPoints() == Points.WIN) {
 			status = Status.TWOPLAYER_WIN;
 		}
 	}
 
 	public boolean isFinished() {
-		return (playerOnePoints == Points.WIN || playerTwoPoints == Points.WIN);
+		return (player1.getPoints() == Points.WIN || player2.getPoints() == Points.WIN);
 	}
 
 	public Status status() {
