e949d91d64c57c9ccb1ee12db5d8af18926ad66f
Carlos B <carlos.baezruiz@gmail.com>
2016-06-15 19:48:48 +0200

    Added first skeleton version to implement philosopher problem

---
+++ philosophers-problem/src/main/java/katas/PhilosopherProblem.java
@@ -0,0 +1,4 @@
+package katas;
+
+public class PhilosopherProblem {
+}
---
+++ philosophers-problem/src/test/java/katas/PhilosopherProblemTest.java
@@ -0,0 +1,11 @@
+package katas;
+
+import org.junit.Test;
+
+public class PhilosopherProblemTest {
+
+	@Test
+	public void newPhilosopher() {
+
+	}
+}
