a07341decc9b23ab3b3d3f85938758fc769dcac1
carlosb <carlos.baezruiz@gmail.com>
2016-06-28 12:59:46 +0200

    Added test to permit eat one philosopher

--- philosophers-problem/src/test/java/katas/concurrent/PhilosopherServiceTablesTest.java
+++ philosophers-problem/src/test/java/katas/concurrent/PhilosopherServiceTablesTest.java
@@ -20,7 +20,7 @@ public class PhilosopherServiceTablesTest {
 	// TODO move to cucumber
 	// TODO create abstract factory
 	@Test(timeout = 2000)
-	public void synchronizedServiceTable() throws IOException, InterruptedException {
+	public void onePhilosopherCannotEat() throws IOException, InterruptedException {
 		MockConcurrentLunchFactory lunchFactory = new MockConcurrentLunchFactory(1);
 		FakeOutPrintStream out = lunchFactory.getFakeOut();
 		final ConcurrentPhilosopher concurrentPhilosopher = lunchFactory.makePhilosopher();
@@ -37,7 +37,26 @@ public class PhilosopherServiceTablesTest {
 		concurrentPhilosopher.leave();
 		assertTrue(out.contains("I am thinking, Philosopher 1"));
 		System.out.println(out.content());
+	}
 
+	@Test(timeout = 2000)
+	public void onePhilosopherCanEat() throws IOException, InterruptedException {
+		MockConcurrentLunchFactory lunchFactory = new MockConcurrentLunchFactory(2);
+		FakeOutPrintStream out = lunchFactory.getFakeOut();
+		final ConcurrentPhilosopher concurrentPhilosopher = lunchFactory.makePhilosopher();
+		// TODO refactorization this object creation
+		List<Philosopher> philosophers = new ArrayList<Philosopher>() {
+			{
+				add(concurrentPhilosopher);
+			}
+		};
+
+		lunch = new PhilosopherLunch(lunchFactory, philosophers);
+		lunch.start();
+		Thread.sleep(100);
+		concurrentPhilosopher.leave();
+		assertTrue(out.contains("I am eating, Philosopher 1"));
+		System.out.println(out.content());
 	}
 
 }
