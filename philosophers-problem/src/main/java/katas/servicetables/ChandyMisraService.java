3ac566dc3832597d0a6c3640a413c13caba60fd8
Carlos B <carlos.baezruiz@gmail.com>
2016-07-02 20:09:36 +0200

    Finished Chandy Misra Solution

--- philosophers-problem/src/main/java/katas/servicetables/ChandyMisraService.java
+++ philosophers-problem/src/main/java/katas/servicetables/ChandyMisraService.java
@@ -1,59 +1,116 @@
 package katas.servicetables;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ChandyMisraService implements ServiceTable {
 	private static class InfoFork {
 		public Boolean available;
 		public Boolean dirty;
+		public int idPhilosopher;
+		public int waitingPhilosopher;
 
 		public InfoFork() {
+			idPhilosopher = -1;
 			available = false;
 			dirty = true;
+			waitingPhilosopher = -1;
+
 		}
 	}
 
 	private final int numberOfForks;
 	private volatile List<InfoFork> forks;
 
-	public ChandyMisraService(int numberOfForks) {
+	public ChandyMisraService(int numberOfForks, int numberOfPhilosophers) {
 		this.numberOfForks = numberOfForks;
-		this.forks = new ArrayList<InfoFork>();
-		for (int i = 0; i < numberOfForks; i++) {
-			this.forks.add(new InfoFork());
+		this.forks = Stream.generate(InfoFork::new).limit(numberOfForks).collect(Collectors.toList());
+
+		/* initialize philosopher */
+		// TODO create a stream for this
+		for (int numFork = 0; numFork < forks.size(); numFork++) {
+			int firstPhilosopher = numFork % numberOfPhilosophers;
+			int secondPhilosopher = (numFork + 1) % numberOfPhilosophers;
+			this.forks.get(numFork).idPhilosopher = firstPhilosopher < secondPhilosopher ? firstPhilosopher : secondPhilosopher;
 		}
+
 	}
 
-	private int[] calculateForks(int numberPhilosopher) {
-		int indexPhilosopher = numberPhilosopher - 1;
+	private int[] calculateForksNeighbours(int indexPhilosopher) {
 		int leftFork = indexPhilosopher;
-		int rightFork = (((indexPhilosopher - 1) % numberOfForks) + numberOfForks) % numberOfForks;
+		int rightFork = ((indexPhilosopher + 1) % numberOfForks);
 		int values[] = { leftFork, rightFork };
 		return values;
 	}
 
-	public boolean tryGetForks(int numberPhilosopher) {
-		if (numberPhilosopher < 0) {
-			return false;
+	public boolean isCorrectNumberOfPhilosopher(int numberPhilosopher) {
+		return (numberPhilosopher >= 0 && forks.size() > 1);
+	}
+
+	private boolean updateFork(int numberFork, int indexPhilosopher) {
+		InfoFork fork = forks.get(numberFork);
+		if (fork.available) {
+			fork.idPhilosopher = indexPhilosopher;
+			fork.available = false;
+		} else {
+			if (isMyFork(indexPhilosopher, fork)) {
+				// TODO test case for give a dirty fork
+				if (checkGiveFork(fork)) {
+					fork.idPhilosopher = indexPhilosopher;
+					return false;
+				}
+				fork.dirty = false;
+				fork.available = false;
+			} else {
+				fork.waitingPhilosopher = indexPhilosopher;
+				return false;
+			}
 		}
-		if (forks.size() <= 1) {
-			return false;
-		}
-		int pairForks[] = calculateForks(numberPhilosopher);
+
 		return true;
+	}
+
+	private boolean checkGiveFork(InfoFork fork) {
+		return fork.waitingPhilosopher != -1 && fork.dirty == true;
+	}
+
+	private boolean isMyFork(int indexPhilosopher, InfoFork fork) {
+		return fork.idPhilosopher == indexPhilosopher;
+	}
+
+	public boolean tryGetForks(int numberPhilosopher) {
+		if (!isCorrectNumberOfPhilosopher(numberPhilosopher)) {
+			return false;
+		}
+
+		int indexPhilosopher = numberPhilosopher - 1;
+		int pairForks[] = calculateForksNeighbours(indexPhilosopher);
+		return (updateFork(pairForks[0], indexPhilosopher) && updateFork(pairForks[1], indexPhilosopher));
 
 	}
 
-	public boolean tryReleaseForks(int numberPhilosopher) {
-		if (numberPhilosopher < 0) {
-			return false;
+	private void releaseFork(int numberFork) {
+		InfoFork fork = forks.get(numberFork);
+		fork.available = true;
+		fork.dirty = true;
+		if (fork.waitingPhilosopher != -1) {
+			fork.idPhilosopher = fork.waitingPhilosopher;
+			fork.waitingPhilosopher = -1;
 		}
-		if (forks.size() <= 1) {
+	}
+
+	public boolean tryReleaseForks(int numberPhilosopher) {
+		if (!isCorrectNumberOfPhilosopher(numberPhilosopher)) {
 			return false;
 		}
 
-		int pairForks[] = calculateForks(numberPhilosopher);
+		int indexPhilosopher = numberPhilosopher - 1;
+		int pairForks[] = calculateForksNeighbours(indexPhilosopher);
+		// TODO test if something it is waiting a second fork
+		releaseFork(pairForks[0]);
+		releaseFork(pairForks[1]);
+
 		return true;
 
 	}
---
+++ philosophers-problem/src/test/java/katas/servicetables/ChandyMisraServiceTest.java
@@ -0,0 +1,57 @@
+package katas.servicetables;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class ChandyMisraServiceTest {
+	private ChandyMisraService chandyMisraService;
+
+	@Test
+	public void lunchWithOnlyOneForkNotwork() {
+		chandyMisraService = new ChandyMisraService(1, 1);
+		assertFalse(chandyMisraService.tryGetForks(1));
+	}
+
+	@Test
+	public void lunchWithIncorrectIndexTryGetForkNotwork() {
+		chandyMisraService = new ChandyMisraService(1, 1);
+		assertFalse(chandyMisraService.tryGetForks(-1));
+	}
+
+	@Test
+	public void lunchWithTwoFork() {
+		chandyMisraService = new ChandyMisraService(2, 1);
+		assertTrue(chandyMisraService.tryGetForks(1));
+	}
+
+	@Test
+	public void lunchWithSameForksNotWork() {
+		chandyMisraService = new ChandyMisraService(2, 1);
+		chandyMisraService.tryGetForks(1);
+		assertTrue(chandyMisraService.tryGetForks(1));
+	}
+
+	@Test
+	public void lunchAndReleaseWithOneForkCorrectlWorks() {
+		chandyMisraService = new ChandyMisraService(2, 1);
+		chandyMisraService.tryGetForks(1);
+		assertTrue(chandyMisraService.tryReleaseForks(1));
+	}
+
+	@Test
+	public void lunchTwoTimesSecondNotWork() {
+		chandyMisraService = new ChandyMisraService(5, 5);
+		chandyMisraService.tryGetForks(4);
+		assertFalse(chandyMisraService.tryGetForks(5));
+	}
+
+	@Test
+	public void lunchAndReleaseFirstFivePhilosophers() {
+		chandyMisraService = new ChandyMisraService(5, 5);
+		chandyMisraService.tryReleaseForks(2);
+		assertTrue(chandyMisraService.tryGetForks(1));
+	}
+
+}
