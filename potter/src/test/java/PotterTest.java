ab59e623564d4887c18a98c00374a5cbd5cc25dd
carlosb1 <mcflurry0@gmail.com>
2017-10-23 22:35:39 +0200

    Refactorization for test

--- potter/src/test/java/PotterTest.java
+++ potter/src/test/java/PotterTest.java
@@ -1,26 +1,32 @@
 
+import org.junit.After;
+import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+
 
 public class PotterTest {
 
     Potter potter;
 
+    @After
+    public void setUp () {
+        potter = new Potter();
+
+    }
+
     @Test
     public void should_be_initialised_empty() {
-            potter = new Potter();
-
             float price = potter.checkout();
-            assertEquals(0.f,price);
+            Assert.assertEquals(0.f,price,0.01);
     }
 
     @Test
     public void should_be_add_one_book () {
-        //TODO add setup
-        potter = new Potter();
-
         potter.add("book1");
         float price = potter.checkout();
+        Assert.assertEquals(8,price,0.01);
     }
+
+
 }
