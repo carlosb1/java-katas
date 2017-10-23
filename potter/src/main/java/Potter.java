8cc58f4b3f05d1adb697f523b2d7c9e12d04e966
carlosb1 <mcflurry0@gmail.com>
2017-10-23 02:38:12 +0200

    Added first skeleton for java kata

---
+++ potter/src/main/java/Potter.java
@@ -0,0 +1,15 @@
+public class Potter {
+    private float price;
+
+    public Potter () {
+
+    }
+
+    public float checkout() {
+        return this.price;
+    }
+
+    public void add(String book1) {
+        this.price = 8.0f;
+    }
+}
---
+++ potter/src/test/java/PotterTest.java
@@ -0,0 +1,27 @@
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PotterTest {
+
+    Potter potter;
+
+    @Test
+    public void should_be_initialised_empty() {
+            potter = new Potter();
+
+            float price = potter.checkout();
+            assertEquals(0.f,price);
+    }
+
+    @Test
+    public void should_be_add_one_book () {
+        //TODO add setup
+        potter = new Potter();
+
+        potter.add("book1");
+        float price = potter.checkout();
+
+    }
+}
