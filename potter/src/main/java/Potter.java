e39e00c4634a59d957fc4074a76a4afbc633e636
carlosb1 <mcflurry0@gmail.com>
2017-10-25 13:24:52 +0200

    Added first test for discount with different books

--- potter/src/main/java/Potter.java
+++ potter/src/main/java/Potter.java
@@ -12,21 +12,31 @@ public class Potter {
 
     private final ArrayList books;
     private float price;
+    private boolean differentBook;
 
     public Potter () {
+        this.differentBook = false;
         this.books = new ArrayList();
     }
 
     public double checkout() {
         long numberBooks = this.books.size();
+        double price = numberBooks*8.0;
 
-        long books1 = this.books.stream().filter(n -> n == Book.BOOK1).count();
-        long books2 = this.books.stream().filter(n -> n == Book.BOOK2).count();
+        //long books1 = this.books.stream().filter(n -> n == Book.BOOK1).count();
+        //long books2 = this.books.stream().filter(n -> n == Book.BOOK2).count();
+        if (differentBook) {
+            price = price * 0.95;
+        }
 
-        return numberBooks*8.0;
+        return price;
     }
 
     public void add(Book book) {
+
+        if (this.books.size() != 0) {
+            this.differentBook = !this.books.contains(book);
+        }
 
         this.books.add(book);
     }
