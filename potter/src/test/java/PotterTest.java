ff3e03422247c30534608939da6d3c9150a799b8
carlosb <carlosb@cvc.uab.es>
2017-10-30 20:23:39 +0100

    Delete test which not provide better coverage

--- potter/src/test/java/PotterTest.java
+++ potter/src/test/java/PotterTest.java
@@ -36,15 +36,7 @@ public class PotterTest {
         double price = potter.checkout();
         Assert.assertEquals(15.2,price,0.0001f);
     }
-
-    @Test
-    public void should_add_three_different_books_with_discount () {
-        potter.add (Potter.Book.BOOK1);
-        potter.add (Potter.Book.BOOK2);
-        potter.add (Potter.Book.BOOK3);
-        double price = potter.checkout();
-        Assert.assertEquals(21.6,price,0.0001f);
-    }
+    
 
     @Test
     public void should_add_three_different_books_not_consecutives_with_discount () {
