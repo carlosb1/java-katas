396890adbf6861ca15b481e9242a29c683cec65c
carlosb1 <mcflurry0@gmail.com>
2017-12-05 21:21:42 +0100

    Added test to find neighbours

--- game_of_life/src/test/java/MapTest.java
+++ game_of_life/src/test/java/MapTest.java
@@ -9,6 +9,14 @@ import java.util.Arrays;
 import static org.junit.Assert.assertEquals;
 
 public class MapTest {
+    public static class Position {
+        protected final int x;
+        protected final int y;
+        public Position (int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
 
     public static final Map addCellula(final Map map, final Cellula ... arrayCellulas ) {
         final List<Cellula> cellulas = Arrays.asList(arrayCellulas);
@@ -17,7 +25,25 @@ public class MapTest {
 
     public static final  Map play(final Map map) {
         final List<Cellula> cellulas = new ArrayList<>();
-        //TODO abstract this function
+        for (Cellula cellula: map.cellulas) {
+            Integer x = cellula.x;
+            Integer y = cellula.y;
+            //TODO abstract this function
+            List<Position> positionsToSearch = Arrays.asList(new Position(x - 1, y - 1), new Position(x, y - 1), new Position(x + 1, y - 1), new Position(x - 1, y + 1), new Position(x, y + 1), new Position(x + 1, y + 1), new Position(x - 1, y), new Position(x + 1, y));
+
+            int numberNeighBours = 0;
+            for (Position position : positionsToSearch) {
+                for (Cellula cellulaToCompare : map.cellulas) {
+                    if (cellulaToCompare.x == position.x && cellulaToCompare.y == position.y) {
+                        numberNeighBours++;
+                    }
+                }
+            }
+            if (numberNeighBours == 2 || numberNeighBours == 3) {
+                cellulas.add(new Cellula(cellula.x, cellula.y));
+            }
+        }
+
         return new Map(cellulas);
 
     }
@@ -52,6 +78,17 @@ public class MapTest {
         final Map  newMap = play(addCellula(map,new Cellula(0,0),new Cellula(0,1)));
         assertEquals(0,newMap.cellulas.size());
     }
+
+    @Test
+    public void should_initialize_three_cellulas_play_and_live () {
+        final Map map = new Map();
+        final Map  newMap = play(addCellula(map,new Cellula(0,0),new Cellula(1,0),new Cellula(1,1)));
+        assertEquals(3,newMap.cellulas.size());
+    }
+
+
+
+
 
 
 
