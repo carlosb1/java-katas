aa5582e38408753e4c2119fce438a81a2d82e2b6
carlosb1 <mcflurry0@gmail.com>
2017-12-05 20:51:46 +0100

    Added test for two different cellulas which are neightbour

--- game_of_life/src/test/java/MapTest.java
+++ game_of_life/src/test/java/MapTest.java
@@ -10,14 +10,23 @@ import static org.junit.Assert.assertEquals;
 
 public class MapTest {
 
-    public static final Map addCellula(final Map map, final Cellula cellula ) {
-        List<Cellula> cellulas = Arrays.asList(cellula);
+    public static final Map addCellula(final Map map, final Cellula ... arrayCellulas ) {
+        final List<Cellula> cellulas = Arrays.asList(arrayCellulas);
         return new Map(cellulas);
     }
 
     public static final  Map play(final Map map) {
         final List<Cellula> cellulas = new ArrayList<>();
-        return new Map(cellulas);
+        if (map.cellulas.size()< 2) {
+            return new Map(cellulas);
+        }
+        Cellula cellula1 = map.cellulas.get(0);
+        Cellula cellula2 = map.cellulas.get(1);
+        //TODO abstract this function
+//        if ((cellula2.y-cellula1.y)==1 && (cellula2.x - cellula1.x) == 0) {
+            return new Map(Arrays.asList(cellula1, cellula2));
+//        }
+
     }
 
 
@@ -42,6 +51,17 @@ public class MapTest {
         final Map  newMap = play(addCellula(map,new Cellula(0,0)));
         assertEquals(0,newMap.cellulas.size());
     }
+
+    @Test
+    public void should_initialize_two_cellulas_play_and_dead () {
+        final Map map = new Map();
+        final Map  newMap = play(addCellula(map,new Cellula(0,0),new Cellula(0,1)));
+        assertEquals(2,newMap.cellulas.size());
+    }
+
+
+
+
 
 
 
