18d1e085041c6bf64142f834dca31ab24f4a7c8f
carlosb1 <mcflurry0@gmail.com>
2017-12-05 20:40:53 +0100

    Added first test to play. It call play function to apply rules

--- game_of_life/src/main/java/models/Cellula.java
+++ game_of_life/src/main/java/models/Cellula.java
@@ -1,8 +1,8 @@
 package models;
 
 public class Cellula {
-    private final Integer x;
-    private final Integer y;
+    public final Integer x;
+    public final Integer y;
     public Cellula(final Integer x, final Integer y) {
             this.x = x;
             this.y = y;
--- game_of_life/src/test/java/MapTest.java
+++ game_of_life/src/test/java/MapTest.java
@@ -1,9 +1,9 @@
+import java.util.ArrayList;
 import java.util.List;
 import models.Cellula;
 import models.Map;
 import org.junit.Test;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 
 import static org.junit.Assert.assertEquals;
@@ -15,6 +15,13 @@ public class MapTest {
         return new Map(cellulas);
     }
 
+    public static final  Map play(final Map map) {
+        final List<Cellula> cellulas = new ArrayList<>();
+        return new Map(cellulas);
+    }
+
+
+
 
 
     @Test
@@ -23,12 +30,18 @@ public class MapTest {
         assertEquals(0,map.cellulas.size());
     }
     @Test
-    public void should_initialize_one_cellula_after_dead () {
+    public void should_initialize_one_cellula () {
         final Map map = new Map();
         final Map  newMap = addCellula(map,new Cellula(0,0));
         assertEquals(1,newMap.cellulas.size());
     }
 
+    @Test
+    public void should_initialize_one_cellula_play_and_dead () {
+        final Map map = new Map();
+        final Map  newMap = play(addCellula(map,new Cellula(0,0)));
+        assertEquals(0,newMap.cellulas.size());
+    }
 
 
 
