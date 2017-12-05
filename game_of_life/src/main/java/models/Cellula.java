dc2cbb2760983465d865253ffc166bc62286879b
carlosb1 <mcflurry0@gmail.com>
2017-12-05 20:11:20 +0100

    Added first two tests to add cellula

---
+++ game_of_life/src/main/java/models/Cellula.java
@@ -0,0 +1,11 @@
+package models;
+
+public class Cellula {
+    private final Integer x;
+    private final Integer y;
+    public Cellula(final Integer x, final Integer y) {
+            this.x = x;
+            this.y = y;
+    }
+
+}
