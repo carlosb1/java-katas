0d66a63db843158d133656f096c60c7c5cb4e18a
carlosb1 <mcflurry0@gmail.com>
2017-12-05 21:37:49 +0100

    Refactorization with java 8

--- game_of_life/src/test/java/MapTest.java
+++ game_of_life/src/test/java/MapTest.java
@@ -26,20 +26,9 @@ public class MapTest {
     public static final  Map play(final Map map) {
         final List<Cellula> cellulas = new ArrayList<>();
         for (Cellula cellula: map.cellulas) {
-            Integer x = cellula.x;
-            Integer y = cellula.y;
-            //TODO abstract this function
-            List<Position> positionsToSearch = Arrays.asList(new Position(x - 1, y - 1), new Position(x, y - 1), new Position(x + 1, y - 1), new Position(x - 1, y + 1), new Position(x, y + 1), new Position(x + 1, y + 1), new Position(x - 1, y), new Position(x + 1, y));
-
-            int numberNeighBours = 0;
-            for (Position position : positionsToSearch) {
-                for (Cellula cellulaToCompare : map.cellulas) {
-                    if (cellulaToCompare.x == position.x && cellulaToCompare.y == position.y) {
-                        numberNeighBours++;
-                    }
-                }
-            }
-            if (numberNeighBours == 2 || numberNeighBours == 3) {
+            List<Position> positionsToSearch = setUpPosicForNeighbours(cellula.x, cellula.y);
+            long numberNeighBours = positionsToSearch.stream().filter(position -> hasItNeighbour(position, map.cellulas)).count();
+            if (isLiveCellula(numberNeighBours)) {
                 cellulas.add(new Cellula(cellula.x, cellula.y));
             }
         }
@@ -48,8 +37,18 @@ public class MapTest {
 
     }
 
+    private static boolean hasItNeighbour(Position position, List<Cellula> cellulas) {
+            return cellulas.stream().filter(cellulaToCompare -> cellulaToCompare.x == position.x && cellulaToCompare.y == position.y).count() >0;
 
+    }
 
+    private static List<Position> setUpPosicForNeighbours(Integer x, Integer y) {
+        return Arrays.asList(new Position(x - 1, y - 1), new Position(x, y - 1), new Position(x + 1, y - 1), new Position(x - 1, y + 1), new Position(x, y + 1), new Position(x + 1, y + 1), new Position(x - 1, y), new Position(x + 1, y));
+    }
+
+    private static boolean isLiveCellula(long numberNeighBours) {
+        return numberNeighBours == 2 || numberNeighBours == 3;
+    }
 
 
     @Test
